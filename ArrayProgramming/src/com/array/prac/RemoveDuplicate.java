5bd7cfc53281e395e357d7ab27441b833926e394
Paresh Jaiswal <pareshjaiswal22@hotmail.com>
2026-08-25 12:46:54 +0530

    Refactor RemoveDuplicate to use Set for uniqueness

--- ArrayProgramming/src/com/array/prac/RemoveDuplicate.java
+++ ArrayProgramming/src/com/array/prac/RemoveDuplicate.java
@@ -1,17 +1,28 @@
+/* 
+Problem Statement: Remove duplicates from an array
+
+Example : a[]= {1, 1, 2, 2, 3, 4, 5}
+output = [1,2,3,4,5]
+
+Step:
+1. Create an empty set
+2. Iterate over an array and add them in the set
+3. convert set to an array.
+4. print the array
+*/
+
 package com.array.prac;
 
 public class RemoveDuplicate {
 	public static void main(String[] args) {
 		int arr [] = {1, 1, 2, 2, 3, 4, 5};
-		for(int i = 0; i<arr.length;i++) {
-			for(int j = i+1; j< arr.length;j++) {
-				if(arr[i] == arr[j]) {
-					arr[j] = 0;
-				}
-			}
-		}
-		for (int i : arr) {
-			System.out.println(i);
-		}
+        Set<Integer> set = new HashSet<>();// Create an empty set
+        for(int i : arr) { // Iterate over an array and add them in the set
+            if(!set.contains(i)) {
+                set.add(i);
+            }
+        }
+        arr = set.stream().mapToInt(i->i).toArray();// convert set to an array
+        System.out.println(Arrays.toString(arr)); // print the array
 	}
 }
